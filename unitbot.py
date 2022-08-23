6d943488476853ad072c8c8e77c7e93ed9695abc
BrainFox <ne0fetch@proton.me>
2022-08-23 11:57:53 +0000

    Update unitbot.py
    I`m sorry for black formater

--- unitbot.py
+++ unitbot.py
@@ -6,10 +6,12 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read("settings.ini")
-
 idd = 0
 
-with TelegramClient("foxx", config["Api"]["api_id"], config["Api"]["api_hash"]) as client:
+
+with TelegramClient(
+    "foxx", config["Api"]["api_id"], config["Api"]["api_hash"]
+) as client:
     get_mine = client.get_me()
 
     @client.on(events.NewMessage(pattern=";chats", outgoing=True))
@@ -25,7 +27,7 @@ with TelegramClient("foxx", config["Api"]["api_id"], config["Api"]["api_hash"]) 
 
     @client.on(events.NewMessage(pattern=";repeater (\w+)", outgoing=True))
     async def set_id(event):
-        # await event.respond('ЧТобы узнать все id пиши ;chats')
+        # await event.respond('Чтобы узнать все id пиши ;chats')
         global idd
         idd = event.pattern_match.group(1)
         await event.edit(f"Ты выбрал {idd}")
@@ -35,7 +37,7 @@ with TelegramClient("foxx", config["Api"]["api_id"], config["Api"]["api_hash"]) 
     async def repeater(event):
         user_from = await event.client.get_entity(event.from_id)
         if int(idd) == user_from.id:
-            await event.respond("Ты в игноре")
+            await event.respond(config["Settings"]["message"])
         else:
             pass
 
@@ -58,69 +60,30 @@ with TelegramClient("foxx", config["Api"]["api_id"], config["Api"]["api_hash"]) 
     @client.on(events.ChatAction())
     async def joiiner(event):
         if event.user_joined:
-            await event.respond(f'{event.user.first_name}, чей Крым?')
-
-    """
-    @client.on(events.NewMessage(pattern=";mag(?: |$)(.*)", outgoing=True))
-    async def magic(event):
-        await event.delete()
-        get_text = event.text[5:]
-
-        for i in get_text:
-            await event.respond(i)
-    """
-
+            await event.respond(
+                f'{event.user.first_name}, {config["Settings"]["joiiner"]}'
+            )
 
     @client.on(events.NewMessage(pattern=";mag(?: |$)(.*)", outgoing=True))
-    async def magic(event):
+    async def magic_first(event):
         get_text = event.text[5:]
-        dict = ''
+        dict = ""
         for i in get_text:
-            if i == ' ':
-                dict += '.'
+            if i == " ":
+                dict += "•"
             await event.edit(f"{dict}{i}")
-            dict = dict + i
+            dict = f"{dict}{i}\n"
             time.sleep(0.553)
-    client.run_until_disconnected()
 
-        ldr = get_text.split(" ", 1)
-        counter = int(ldr[0])
-        mesg = ldr[1]
-        for i in range(counter):
-            await event.respond(mesg)
+    # @client.on(events.NewMessage(pattern=";mag(?: |$)(.*)", outgoing=True))
+    # async def magic_second(event):
+    #     get_text = event.text[5:]
+    #     dict = ''
+    #     for i in get_text:
+    #         if i == ' ':
+    #             dict += '.'
+    #         await event.edit(f"{dict}{i}")
+    #         dict = dict + i
+    #         time.sleep(0.553)
 
-    @client.on(events.NewMessage(pattern=";ran (\w+)", outgoing=True))
-    async def randomer(event):
-        get_count = int(event.pattern_match.group(1))
-        randomizee = random.randint(0, get_count)
-        await event.edit(str(randomizee))
-
-    """
-    @client.on(events.ChatAction())
-    async def joiiner(event):
-        if event.user_joined:
-            await event.respond(f'{event.user.first_name}, welcum')
-    """
-    
-    """
-    @client.on(events.NewMessage(pattern=";mag(?: |$)(.*)", outgoing=True))
-    async def magic(event):
-        await event.delete()
-        get_text = event.text[5:]
-
-        for i in get_text:
-            await event.respond(i)
-    """
-
-
-    @client.on(events.NewMessage(pattern=";mag(?: |$)(.*)", outgoing=True))
-    async def magic(event):
-        get_text = event.text[5:]
-        dict = ''
-        for i in get_text:
-            if i == ' ':
-                dict += '.'
-            await event.edit(f"{dict}{i}")
-            dict = dict + i
-            time.sleep(0.553)
     client.run_until_disconnected()
