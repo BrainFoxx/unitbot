f31f38915920d5582edc1b700895e9b8f5bd9603
BrainFox <ne0fetch@proton.me>
2022-11-07 16:42:01 +0000

    Update this bot
    Replace in "repeater" function.
    Now you can write ";repeater <and your message>"

--- unitbot.py
+++ unitbot.py
@@ -1,5 +1,5 @@
-from telethon import TelegramClient
 from telethon.sync import events
+from telethon import TelegramClient
 import time
 import random
 import configparser
@@ -12,7 +12,7 @@ idd = 0
 with TelegramClient(
     "foxx", config["Api"]["api_id"], config["Api"]["api_hash"]
 ) as client:
-    
+
 
     @client.on(events.NewMessage(pattern=";chats", outgoing=True))
     async def chatss(event):
@@ -21,23 +21,24 @@ with TelegramClient(
             gg = f"{dialog.name} : {dialog.id}"
             liist.append(gg)
         await event.delete()
-        await event.respond("Все твои чаты: ")
+        await event.respond("All your chats: ")
         await event.respond(str(liist))
         print(idd)
 
     @client.on(events.NewMessage(pattern=";repeater (\w+)", outgoing=True))
     async def set_id(event):
-        # await event.respond('Чтобы узнать все id пиши: ;chats')
         global idd
-        idd = event.pattern_match.group(1)
-        await event.edit(f"Ты выбрал {idd}")
-        # await event.respond(f'Ты выбрал {idd}')
+        global text
+        text = event.text[9:]
+        idd = str(event.original_update.user_id)
+        await event.delete()
+        await client.send_message("me", f'Your choose is {idd}')
 
     @client.on(events.NewMessage(func=lambda e: e.is_private, incoming=True))
     async def repeater(event):
         user_from = await event.client.get_entity(event.from_id)
         if int(idd) == user_from.id:
-            await event.respond(config["Settings"]["message"])
+            await event.respond(text)
         else:
             pass
 
@@ -56,13 +57,6 @@ with TelegramClient(
         get_count = int(event.pattern_match.group(1))
         randomizee = random.randint(0, get_count)
         await event.edit(str(randomizee))
-
-    @client.on(events.ChatAction())
-    async def joiiner(event):
-        if event.user_joined:
-            await event.respond(
-                f'{event.user.first_name}, {config["Settings"]["joiiner"]}'
-            )
 
     @client.on(events.NewMessage(pattern=";mag(?: |$)(.*)", outgoing=True))
     async def magic_first(event):
@@ -86,5 +80,5 @@ with TelegramClient(
     #         dict = dict + i
     #         time.sleep(0.553)
 
-    
+
     client.run_until_disconnected()
